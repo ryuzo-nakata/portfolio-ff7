e02fea65b1257a5a9841907be84cd0b9e2df06dc
ryuzo <ryu.thx2u@gmail.com>
2019-12-10 15:45:54 +0900

    create materia css

--- components/parts/MateriaParts.vue
+++ components/parts/MateriaParts.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="wrapper">
-    <div class="tapioka" />
+    <div class="materia" />
+    <div class="downlight1" />
     <div class="highlight2" />
-    <div class="highlight4" />
+    <div class="highlight3" />
     <!-- <div class="shadow" />
     <div class="dropshadow" /> -->
   </div>
@@ -35,49 +36,54 @@ body {
   height: 20px;
   position: relative;
 }
-.tapioka {
+.materia {
   width: 20px;
   height: 20px;
   z-index: 0;
   border-radius: 50%;
   background: radial-gradient(
     closest-side at 49% 49%,
-    rgba(4, 4, 0, 1) 0%,
-    rgba(25, 21, 21, 1) 75%,
-    rgba(90, 77, 77, 1) 92%,
-    rgba(150, 135, 135, 0.6) 98%
+    rgba(150, 150, 150, 1) 0%,
+    rgba(100, 21, 21, 1) 30%
   );
+}
+.downlight1 {
+  position: absolute;
+  top: 10%;
+  left: 15%;
+  z-index: 1;
+  width: 12px;
+  height: 12px;
+  background: rgba(0, 0, 0, 1);
+  border-radius: 50%;
+  filter: blur(2px);
+  opacity: 0.6;
 }
 .highlight2 {
   position: absolute;
-  top: 25%;
-  left: 30%;
+  top: 20%;
+  left: 25%;
   z-index: 1;
-  width: 6px;
-  height: 2px;
+  width: 3px;
+  height: 1px;
   border-radius: 50%;
   border-top: 0.5px solid #fff;
   transform: rotate(-70deg) scaleX(0.9) scaleY(1.5) skewY(18deg);
   filter: blur(0.6px);
 }
-.highlight4 {
+.highlight3 {
   position: absolute;
-  top: 38%;
-  left: 30%;
-  z-index: 1;
-  width: 12.2px;
-  height: 9px;
-  background: radial-gradient(
-    farthest-side at center,
-    rgba(255, 255, 255, 1) 0%,
-    rgba(255, 255, 255, 1) 64%,
-    rgba(194, 132, 132, 0) 100%
-  );
+  top: 0%;
+  left: 0%;
+  width: 18px;
+  height: 18px;
+  background-color: transparent;
+  box-shadow: inset -3px -6px 0 -3px rgba(255, 255, 255, 1);
   border-radius: 50%;
-  transform: rotate(-20deg);
-  filter: blur(2px);
+  filter: blur(1px);
   opacity: 0.15;
 }
+
 .shadow {
   position: absolute;
   top: 69%;
